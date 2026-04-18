f62522a3fd7cc3841fe5d35e66160bddfe681730
kabirarora108 <kabir.arora108@gmail.com>
2026-04-18 15:13:39 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (64.68%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        # 1. Map the Roman numerals to integers
+        roman_map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        n = len(s)
+        
+        # 2. Iterate through the string
+        for i in range(n):
+            # Check if this is a subtraction case (e.g., IV, IX, XL)
+            if i + 1 < n and roman_map[s[i]] < roman_map[s[i+1]]:
+                total -= roman_map[s[i]]
+            else:
+                total += roman_map[s[i]]
+                
+        return total
